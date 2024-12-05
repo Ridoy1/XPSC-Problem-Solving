0338973b50e5e298e97b09cf14e633c79a665b9c
Ridoy karmakar <76881301+Ridoy1@users.noreply.github.com>
2024-12-05 23:39:42 +0600

    Add files via upload
    add code

---
+++ week6/Day5/A_Little_Nikita.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        int d = abs(n - m);
+        if (n >= m)
+        {
+            if (d % 2 == 0)
+            {
+                cout << "Yes" << endl;
+            }
+            else
+            {
+                cout << "No" << endl;
+            }
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+
+    return 0;
+}
