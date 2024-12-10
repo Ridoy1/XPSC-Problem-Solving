96f2edf52dbc616f995ce076977e3afcc56447c7
Ridoy karmakar <76881301+Ridoy1@users.noreply.github.com>
2024-12-10 23:34:51 +0600

    Add files via upload
    add code

---
+++ week7/Day3/B_Petr_and_a_Combination_Lock.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    bool ok = false;
+    for (int m = 0; m < (1 << n); m++)
+    {
+        int sum = 0;
+        for (int k = 0; k < n; k++)
+        {
+            if ((m >> k) & 1)
+            {
+                sum += a[k];
+            }
+            else
+            {
+                sum -= a[k];
+            }
+        }
+        if (sum % 360 == 0)
+        {
+            ok = true;
+            break;
+        }
+    }
+    if (ok)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
