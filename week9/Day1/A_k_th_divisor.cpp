05318b832b69ed0a9b75d9ded010804942bb3b52
Ridoy karmakar <76881301+Ridoy1@users.noreply.github.com>
2024-12-23 00:46:49 +0600

    Add files via upload
    add code

--- week9/Day1/A_k_th_divisor.cpp
+++ week9/Day1/A_k_th_divisor.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 int main()
 {
-    int n, k;
+    long long n, k;
+    cin >> n >> k;
+    vector<long long> v;
+    for (long long i = 1; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            v.push_back(i);
+            if ((n / i) != i)
+                v.push_back(n / i);
+        }
+    }
+    sort(v.begin(), v.end());
+    if (v.size() < k)
+    {
+        cout << -1 << '\n';
+    }
+    else
+    {
+        cout << v[k - 1] << '\n';
+    }
+
     return 0;
 }
