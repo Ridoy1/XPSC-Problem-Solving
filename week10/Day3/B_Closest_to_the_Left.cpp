e2e779c96975d0b229f608d97c32f97b1c8ffba4
Ridoy karmakar <76881301+Ridoy1@users.noreply.github.com>
2024-12-31 22:57:31 +0600

    Add files via upload
    add code

---
+++ week10/Day3/B_Closest_to_the_Left.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < q; i++)
+    {
+        int k, l = 0, r = n - 1, m, ans = 0;
+        cin >> k;
+        while (l <= r)
+        {
+            m = (l + r) / 2;
+            if (k >= a[m])
+            {
+                ans = m + 1;
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
